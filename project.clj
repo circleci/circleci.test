50a78e4b3c261dda491cefa3f2dd97a96bf4437b
Gordon Syme <gordonsyme@gmail.com>
2017-03-16 16:29:31 +0000

    Port our JunitReporter to circleci.test

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :pedantic? :abort
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.xml "0.0.8"]]
   :profiles {:uberjar {:aot :all}})
---
+++ test/circleci/test/report/test_junit.clj
@@ -0,0 +1,49 @@
+(ns circleci.test.report.test-junit
+  (:require [clojure.test :refer (deftest testing is)]
+            circleci.test
+            circleci.test.report
+            [circleci.test.report.junit :as junit])
+  (:import clojure.data.xml.Element))
+
+(deftest failure-works
+  (is (instance? Element (#'junit/failure-xml
+                          {:expected (= 3 (+ 1 1)),
+                           :message nil,
+                           :type :fail,
+                           :actual (not (= 3 2)),
+                           :file "test_junit.clj",
+                           :line 8}))))
+
+(deftest testcase-works
+  (let [ret (#'junit/testcase-xml {:type :end-test-var
+                                   :name #'clojure.core/map
+                                   :elapsed 0.03}
+                                  (junit/failure-xml
+                                   {:expected (= 3 (+ 1 1)),
+                                    :message nil,
+                                    :type :fail,
+                                    :actual (not (= 3 2)),
+                                    :file "test_junit.clj",
+                                    :line 8}))]
+    (is (instance? Element ret))
+    (testing "it has a time"
+      (is (-> ret :attrs :time float?)))))
+
+(deftest testsuite-works
+  (is (instance? Element
+                 (#'junit/suite-xml {:ns (find-ns 'clojure.core)}
+                                    [(#'junit/testcase-xml {:type :end-test-var
+                                                            :name #'clojure.core/map
+                                                            :elapsed 0.03}
+                                                           [(junit/failure-xml
+                                                             {:expected (= 3 (+ 1 1)),
+                                                              :message nil,
+                                                              :type :fail,
+                                                              :actual (not (= 3 2)),
+                                                              :file "test_junit.clj",
+                                                              :line 8})])]))))
+
+(deftest junit-doesn't-break-return-code
+  ;; `lein test` assumes tests return a map.
+  (binding [circleci.test.report/*reporters* [(junit/reporter (atom []) (atom []))]]
+    (is (map? (circleci.test/run-tests 'circleci.test-config)))))
