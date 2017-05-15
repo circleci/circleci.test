c6dfc461d51ade53373cfa425f4202a3cc8b234b
Gordon Syme <gordonsyme@gmail.com>
2017-05-15 10:56:16 +0100

    Add sample config that instantiates test reporters

--- dev-resources/circleci_test/config.clj
+++ dev-resources/circleci_test/config.clj
@@ -1,2 +1,8 @@
+(require '[circleci.test.report :refer (clojure-test-reporter)])
+(require '[circleci.test.report.junit :as junit])
+
 {:selectors {:all (constantly true)
-             :default (complement :failing)}}
+             :default (complement :failing)}
+ :reporters [(clojure-test-reporter)
+             (junit/reporter (or (System/getenv "CIRCLE_TEST_REPORTS")
+                                 "."))]}
